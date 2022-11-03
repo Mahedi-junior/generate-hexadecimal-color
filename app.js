0578b9522e89017716ac94d3cfee08f5435aa265
Mahedi hasan <mahedi2112p@gmail.com>
2022-11-03 23:41:30 +0600

    added copy color modal

---
+++ app.js
@@ -0,0 +1,72 @@
+/* 
+* Project Requirements:
+1. Change the background color by generating random rgb color by clicking a button
+2. also display the hex code to a disabled input field
+3. add a button to copy to color code
+4. add toast message when copied color code
+5.
+
+*/
+// steps
+
+// global
+let div = null;
+// 1. create onload handler
+window.onload = () => {
+    main()
+}
+
+function main() {
+    const root = document.getElementById("root");
+    const changeBtn = document.getElementById("change-btn");
+    const output = document.getElementById("output");
+    const copyBtn = document.getElementById("copy-btn");
+    changeBtn.addEventListener("click", function () {
+        const bgColor = generateHexColor();
+        root.style.backgroundColor = bgColor;
+        output.value = bgColor;
+    });
+
+    copyBtn.addEventListener("click", function () {
+        navigator.clipboard.writeText(output.value);
+        if (div !== null) {
+            div.remove();
+            div = null;
+        }
+        generateToastMessage(`${output.value} copied`);
+    });
+}
+
+// 2. random color generator function
+function generateHexColor() {
+    // 255,255,255 ---> hexCode---->#ffffff
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
+
+function generateToastMessage(msg) {
+    div = document.createElement("div");
+    div.innerText = msg;
+    div.className = "toast-message toast-message-slide-in";
+
+    div.addEventListener("click", function () {
+        div.classList.remove("toast-message-slide-in");
+        div.classList.add("toast-message-slide-out");
+
+        div.addEventListener("animationend", function () {
+            div.remove();
+            div = null;
+        });
+    });
+
+    document.body.appendChild(div);
+}
+// step
+// 3. collect all necessary references
+// 4. handle the change button click event
+// 5. handle the copy button click event
+// 6. active toast message
+// 7. create dynamic toast message
+// 8.
